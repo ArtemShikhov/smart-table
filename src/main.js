7e5e55e2f1abb22b6150a66d62ac1b3cb8c25b13
Artem Shikhov <Artem.s.vvvv@mail.ru>
2026-04-13 10:59:00 +0500

    feat: поиск и сортировка через сервер

--- src/main.js
+++ src/main.js
@@ -42,9 +42,9 @@ async function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let query = {}; // здесь будут формироваться параметры запроса
     // @todo: использование
-    // query = applySearching(query, state, action);
+    query = applySearching(query, state, action);
     query = applyFiltering(query, state, action);
-    // query = applySorting(query, state, action);
+    query = applySorting(query, state, action);
     query = applyPagination(query, state, action); // обновляем query
 
     const { total, items } = await api.getRecords(query); // запрашиваем данные с собранными параметрами
